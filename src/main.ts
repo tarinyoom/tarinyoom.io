21d042fe11547f0955e2c3de373d7d5917f9ffcd
Adam C Reynolds <adam.chiu.reynolds@gmail.com>
2025-07-22 13:52:09 -0700

    Manually hoist article array

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { marked } from "marked";
 
+const articles = [ "/articles/a001_hello.md", "/articles/a002_another.md" ];
+
 function h<K extends keyof HTMLElementTagNameMap>(
   tag: K,
   props: Partial<HTMLElementTagNameMap[K]> = {},
@@ -116,7 +118,6 @@ function renderArticle(articlePromise: Promise<Article>): HTMLElement {
 }
 
 function renderMainContent(): HTMLElement {
-  const articles = [ "/articles/a001_hello.md", "/articles/a002_another.md" ];
   const articleElements = articles.map(fetchArticle).map(renderArticle);
   return h("main", {}, [
     h("div", { className: "content" }, articleElements)
